85c83dda46fa52169a136993fd1ca74ee890b822
Cristina Diaz <diazcristina.la@gmail.com>
2022-06-30 15:34:11 -0700

    commit tree dea806d05228282f0e62e6cc9e20e9e96444ef44

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,23 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ["Fiction","Non-Fiction" ]}
+    validate :is_clickbait
+  
+
+    def is_clickbait
+
+        titles =  ["Won't Believe", "Secret", "Top [number]", "Guess" ]
+        titles.each |title| do 
+        
+
+            unless titles.include? (title)
+                errors.add( :title, "not clickbait" )
+         
+            end
+        end
+    end
+ 
+
 end
